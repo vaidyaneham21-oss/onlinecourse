3b99dbcb6a91a3ba46cc93a56881dd875bc901f2
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-02 14:34:33 +0530

    Refactor models to add Instructor and Learner classes
    Refactored models to include Instructor and Learner classes. Updated Course and Submission models to associate with Instructor and Learner respectively.

--- models.py
+++ models.py
@@ -1,39 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Learner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 class Course(models.Model):
     name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.title
-
-
 class Question(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.question_text
-
+    def is_get_score(self, selected_ids):
+        correct_choices = self.choice_set.filter(is_correct=True)
+        if set([c.id for c in correct_choices]) == set(selected_ids):
+            return 1
+        return 0
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.choice_text
-
-
 class Submission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
+    learner = models.ForeignKey(Learner, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
